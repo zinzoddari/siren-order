a0009ded47033d40862c06632db0d48d988db98e
Jwhyee <kawnsdud@naver.com>
2023-09-20 11:51:26 +0900

    project init

---
+++ src/main/java/com/project/springapistudy/SpringApiStudyApplication.java
@@ -0,0 +1,13 @@
+package com.project.springapistudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiStudyApplication.class, args);
+    }
+
+}
